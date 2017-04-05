869880894a1c0dc61042df74ca5c530e6b81f06f
Nicu <nicurotaru89@gmail.com>
2017-04-05 07:26:02 +0400

    model new updates

--- local/Siru/Mobile/Model/Payment.php
+++ local/Siru/Mobile/Model/Payment.php
@@ -6,18 +6,20 @@
 class Siru_Mobile_Model_Payment extends Mage_Payment_Model_Method_Abstract
 {
     /**
-     *
+     * Siru_Mobile_Model_Payment constructor.
      */
     public function __construct()
     {
         $this->includes();
         $this->wc_siru_disable_on_order_total();
+        $this->wc_siru_verify_ip();
     }
 
     /**
      * @var string
      */
     protected $_code = 'siru_mobile';
+
     protected $_formBlockType = 'siru_mobile/form_desc';
 
     /**
@@ -30,6 +32,23 @@ class Siru_Mobile_Model_Payment extends Mage_Payment_Model_Method_Abstract
      */
     public function getOrderPlaceRedirectUrl()
     {
+//        $paymentInfo = $this->getInfoInstance();
+
+        $quoteId = Mage::getSingleton('checkout/session')->getQuoteId();
+
+        $quote = Mage::getModel("sales/quote")->load($quoteId);
+
+        $order = Mage::getModel('sales/order')->load($quoteId);
+
+//        $order->setState(Mage_Sales_Model_Order::STATE_COMPLETE, true)->save();
+//        echo '<pre>';
+
+//        print_r($quote);
+//        exit;
+
+
+        $customer = $quote->_data;
+
         $data = Mage::getStoreConfig('payment/siru_mobile');
 
         $merchantId = $data['merchant_id'];
@@ -55,13 +74,17 @@ class Siru_Mobile_Model_Payment extends Mage_Payment_Model_Method_Abstract
 
         try {
 
-
+            $total = $quote->_data['base_grand_total'];
+            $total = number_format($total, 2);
 
             $taxClass = (int)$data['tax_class'];
 
             // Siru variant2 requires price w/o VAT
             // To avoid decimal errors, deduct VAT from total instead of using $order->get_subtotal()
-
+            $taxPercentages = array(1 => 0.1, 2 => 0.14, 3 => 0.24);
+            if (isset($taxPercentages[$taxClass]) == true) {
+                $total = bcdiv($total, $taxPercentages[$taxClass] + 1, 2);
+            }
 
             $serviceGroup = $data['service_group'];
             $instantPay = $data['instant_payment'];
@@ -80,8 +103,8 @@ class Siru_Mobile_Model_Payment extends Mage_Payment_Model_Method_Abstract
                 ->set('description', 'Magento ')
                 ->createPayment();
 
-
-            return $transaction['redirect'];
+//            return Mage::getUrl('siru_mobile/payment/redirect', array('_secure' => false));
+//            return $transaction['redirect'];
 
         } catch (\Siru\Exception\InvalidResponseException $e) {
             error_log('Siru Payment Gateway: Unable to contact payment API. Check credentials.');
@@ -142,6 +165,52 @@ class Siru_Mobile_Model_Payment extends Mage_Payment_Model_Method_Abstract
         return;
     }
 
+    /**
+     * Checks if users IP-address is allowed to make mobile payments. If not, remove Siru from payment options.
+     */
+    private function wc_siru_verify_ip()
+    {
+        if ($this->_canUseCheckout == true) {
+            $ip = Mage::helper('core/http')->getRemoteAddr();
+
+//            $cache = (array) get_transient('wc_siru_ip_check');
+            $data = Mage::getStoreConfig('payment/siru_mobile');
+
+            $merchantId = $data['merchant_id'];
+            $secret = $data['merchant_secret'];
+
+            $signature = new \Siru\Signature($merchantId, $secret);
+
+            $api = new \Siru\API($signature);
+
+            // Use production endpoint if configured by admin
+            $endPoint = $data['live_environment'];
+
+
+            if (!$endPoint) {
+                $api->useStagingEndpoint();
+            }
+
+            try {
+
+                $allowed = $api->getFeaturePhoneApi()->isFeaturePhoneIP($ip);
+
+                // Cache result for one houre
+//            $cache[$ip] = $allowed;
+//            set_transient('wc_siru_ip_check', $cache, 3600);
+
+                if ($allowed == false) {
+                    $this->_canUseCheckout = false;
+                }
+
+            } catch (\Siru\Exception\ApiException $e) {
+                error_log(sprintf('Siru Payment Gateway: Unable to verify if %s is allowed to use mobile payments. %s', $ip, $e->getMessage()));
+            }
+
+        }
+
+        return;
+    }
 
 
 }
--- local/Siru/Mobile/Model/TaxClass.php
+++ local/Siru/Mobile/Model/TaxClass.php
@@ -12,11 +12,10 @@ class Siru_Mobile_Model_TaxClass
     public function toOptionArray()
     {
         return array(
-                array('value'=>0, 'label'=>0),
-                array('value'=>1, 'label'=>1),
-                array('value'=>2, 'label'=>2),
-                array('value'=>3, 'label'=>3)
-
+            array('value' => 0, 'label' => 0),
+            array('value' => 1, 'label' => 1),
+            array('value' => 2, 'label' => 2),
+            array('value' => 3, 'label' => 3)
         );
     }
 
