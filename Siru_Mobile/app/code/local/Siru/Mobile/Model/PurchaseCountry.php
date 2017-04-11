2275e08b2c34a04c7ba42f39c058801eebd21984
Joni Halme <joni.halme@sirumobile.com>
2017-04-11 15:23:10 +0300

    improved logging in notifications

---
+++ Siru_Mobile/app/code/local/Siru/Mobile/Model/PurchaseCountry.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Class Siru_Mobile_Model_purchaseCountry
+ */
+class Siru_Mobile_Model_PurchaseCountry
+{
+    /**
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        return array(
+            array('value'=>'FI', 'label'=>'FI')
+        );
+    }
+
+}
