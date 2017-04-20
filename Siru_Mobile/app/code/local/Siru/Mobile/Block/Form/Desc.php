6775f2dabeeb3aa6b28aaec2a0e6e6322f86f189
Joni Halme <joni.halme@sirumobile.com>
2017-04-20 16:20:03 +0300

    removed unused code

--- Siru_Mobile/app/code/local/Siru/Mobile/Block/Form/Desc.php
+++ Siru_Mobile/app/code/local/Siru/Mobile/Block/Form/Desc.php
@@ -11,6 +11,5 @@ class Siru_Mobile_Block_Form_Desc extends Mage_Payment_Block_Form
     protected function _construct()
     {
         parent::_construct();
-        $this->setTemplate('sirumobile/desc.phtml');
     }
 }
